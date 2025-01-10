bf1dfc75efac5f2dfae2a0efa9ea00fc6a70076b
Maarten Staa <maartens@spotify.com>
2025-01-10 16:01:21 +0100

    Add debugging with dap

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,23 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
+    "mfussenegger/nvim-dap-python",
+    'theHamsta/nvim-dap-virtual-text',
+  },
+  config = function ()
+    require("dap-python").setup("uv")
+    require("dapui").setup()
+    require("nvim-dap-virtual-text").setup({
+	    virt_text_pos = 'inline',
+    })
+
+    vim.keymap.set('n', '<leader>b', function() require('dap').toggle_breakpoint() end)
+    vim.keymap.set('n', '<leader>c', function()
+      require('dap').continue()
+      require('dapui').open()
+    end)
+    vim.keymap.set('n', '<leader>dt', function() require('dapui').toggle() end)
+  end
+}
