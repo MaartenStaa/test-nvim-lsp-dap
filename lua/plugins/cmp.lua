f5867f63f0567bbfa4c25265c7d0425e3602bfdb
Maarten Staa <maartens@spotify.com>
2025-01-10 15:40:05 +0100

    Add autocomplete with cmp

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,33 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-buffer",
+  },
+  config = function()
+    local cmp = require('cmp')
+    cmp.setup{
+      snippet = {
+	expand = function(args)
+	  vim.snippet.expand(args.body)
+	end
+      },
+      mapping = cmp.mapping.preset.insert({
+	['<C-b>'] = cmp.mapping.scroll_docs(-4),
+	['<C-f>'] = cmp.mapping.scroll_docs(4),
+	['<C-Space>'] = cmp.mapping.complete(),
+	['<C-e>'] = cmp.mapping.abort(),
+	['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+      }),
+      sources = cmp.config.sources({
+	{ name = 'nvim_lsp' },
+	{ name = 'vsnip' }, -- For vsnip users.
+	-- { name = 'luasnip' }, -- For luasnip users.
+	-- { name = 'ultisnips' }, -- For ultisnips users.
+	-- { name = 'snippy' }, -- For snippy users.
+      }, {
+	{ name = 'buffer' },
+      })
+    }
+  end
+}
